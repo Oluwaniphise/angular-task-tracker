aac3f68a3bf41538af2719990c99c0c376ad7d51
Oduyale Enoch <oduyaleenoch@gmail.com>
2023-10-11 17:56:40 +0100

    created services

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { TASKS } from 'src/app/mock-task';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/TASK';
 
 @Component({
@@ -7,6 +7,14 @@ import { Task } from 'src/app/TASK';
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS
+export class TasksComponent implements OnInit {
+  tasks: Task[] = []
+
+  constructor(private taskService: TaskService){}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) =>{
+      this.tasks = tasks
+    })
+  }
 }
